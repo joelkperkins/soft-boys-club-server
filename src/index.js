11ae51772c1dce524e84b17f956432b7cd76cd29
Oomrazoom <joel.climbs@gmail.com>
2020-07-22 22:31:51 -0600

    / route

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // ping
+app.get('/', (req, res) => res.status(200).send('BE SOFT'))
 app.get('/ping', (req, res) => res.status(200).send('OK'))
 
 // routes
